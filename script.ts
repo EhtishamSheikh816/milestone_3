3fb5a6422b74d41342fa84b53a5379e53c70cf3c
Ehtisham_Sheikh <160014843+EhtishamSheikh816@users.noreply.github.com>
2024-10-18 12:37:15 -0700

    main

---
+++ script.ts
@@ -0,0 +1,54 @@
+document
+  .getElementById("resumeForm")
+  ?.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const nameElement = document.getElementById("name") as HTMLInputElement;
+    const emailElement = document.getElementById("email") as HTMLInputElement;
+    const phoneElement = document.getElementById("phone") as HTMLInputElement;
+    const educationElement = document.getElementById(
+      "education"
+    ) as HTMLTextAreaElement;
+    const experienceElement = document.getElementById(
+      "experience"
+    ) as HTMLTextAreaElement;
+    const skillsElement = document.getElementById(
+      "skills"
+    ) as HTMLTextAreaElement;
+
+    if (
+      nameElement &&
+      emailElement &&
+      phoneElement &&
+      educationElement &&
+      experienceElement &&
+      skillsElement
+    ) {
+      const name = nameElement.value;
+      const email = emailElement.value;
+      const phone = phoneElement.value;
+      const education = educationElement.value;
+      const experience = experienceElement.value;
+      const skills = skillsElement.value;
+
+      const resumeOutput = `<h2>Resume</h2>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Phone:</strong> ${phone}</p>
+      <h3>Education</h3>
+      <p>${education}</p>
+      <h3>Work Experience</h3>
+      <p>${experience}</p>
+      <h3>Skills</h3>
+      <p>${skills}</p>`;
+
+      const resumeOutputElement = document.getElementById("resumeOutput");
+      if (resumeOutputElement) {
+        resumeOutputElement.innerHTML = resumeOutput;
+      } else {
+        console.error("The resume Output element are missing.");
+      }
+    } else {
+      console.error("One or more fields are missing.");
+    }
+  });
